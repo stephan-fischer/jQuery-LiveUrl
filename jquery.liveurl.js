84138fc7b1a43d57fe552110d7f956cf31280d47
Emiliano Zilocchi <ezilocchi@gmail.com>
2013-07-12 12:31:20 -0300

    Add Feature: Set config based on template

--- jquery.liveurl.js
+++ jquery.liveurl.js
@@ -14,6 +14,10 @@
   var initComponent = function(options) {
     var o = options;
 
+    if(typeof o.template === 'string') {
+      o.container.html(o.template);
+    }
+
     return {
       already: [],
 
@@ -291,6 +295,83 @@
   };
 
   var defaults = {
+    curImages : new Array(),
+    loadStart : function() {
+      this.container.find('.liveurl-loader').show();
+    },
+    loadEnd : function() {
+      this.container.find('.liveurl-loader').hide();
+    },
+    success: function(data) {
+      var output = this.container.find('.liveurl');
+
+      output.find('.title').text(data.title);
+      output.find('.description').text(data.description);
+      output.find('.url').text(data.url);
+      output.find('.image').empty();
+
+      output.find('.close').one('click', function() {
+        var liveUrl = $(this).parent();
+        liveUrl.hide('fast');
+        liveUrl.find('.video').html('').hide();
+        liveUrl.find('.image').html('');
+        liveUrl.find('.controls .prev').addClass('inactive');
+        liveUrl.find('.controls .next').addClass('inactive');
+        liveUrl.find('.thumbnail').hide();
+        liveUrl.find('.image').hide();
+
+        $('textarea').trigger('clear'); 
+        this.curImages = new Array();
+      });
+
+      output.show('fast');
+
+      if (data.video != null) {
+        var ratioW = data.video.width / 350;
+        data.video.width  = 350;
+        data.video.height = data.video.height / ratioW;
+
+        var video =
+        '<object width="' + data.video.width  + '" height="' + data.video.height  + '">' +
+        '<param name="movie"' +
+        'value="' + data.video.file  + '"></param>' +
+        '<param name="allowScriptAccess" value="always"></param>' +
+        '<embed src="' + data.video.file  + '"' +
+        'type="application/x-shockwave-flash"' +
+        'allowscriptaccess="always"' +
+        'width="' + data.video.width  + '" height="' + data.video.height  + '"></embed>' +
+        '</object>';
+        output.find('.video').html(video).show();
+      }
+    },
+    addImage: function(image) {
+      var output  = this.container.find('.liveurl');
+      var jqImage = $(image);
+      jqImage.attr('alt', 'Preview');
+
+      if ((image.width / image.height) > 7 || (image.height / image.width) > 4 ) {
+        // we dont want extra large images...
+        return false;
+      }
+
+      this.curImages.push(jqImage.attr('src'));
+      output.find('.image').append(jqImage);
+
+      if (this.curImages.length == 1) {
+        // first image...
+
+        output.find('.thumbnail .current').text('1');
+        output.find('.thumbnail').show();
+        output.find('.image').show();
+        jqImage.addClass('active');
+      }
+
+      if (this.curImages.length == 2) {
+        output.find('.controls .next').removeClass('inactive');
+      }
+
+      output.find('.thumbnail .max').text(this.curImages.length);
+    },
     meta: [
       ['description','name',     'description'],
       ['description','property', 'og:description'],
@@ -311,12 +392,8 @@
     minWidth         : 100,
     minHeight        : 32,
     logoWord         : 'logo',
-    success          : function() {},
-    loadStart        : function() {},
-    loadEnd          : function() {},
     imgLoadStart     : function() {},
-    imgLoadEnd       : function() {},
-    addImage         : function() {}
+    imgLoadEnd       : function() {}
   }
 
   $.fn.extend({
